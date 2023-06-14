bd3f9b59c6d3d7d17fc03227bfa1fc42cb764c23
hardik3018 <hardik.mca21.du@gmail.com>
2023-06-14 20:33:40 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './components/navBar';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import './bootstrap/dist/css/bootstrap.min.css'
 import { Banner } from './components/banner';
 import {Skills} from './components/Skills';
 import {Example } from './components/Example';
